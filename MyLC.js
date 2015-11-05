c1496973f702f88f31a641bd30de2fc2d582bf24
Duncan Rowland <duncan.a.rowland@gmail.com>
2015-11-05 10:18:14 +0000

    First commit

---
+++ MyLC.js
@@ -0,0 +1,35 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      // Show in alphabetical order
+      return Tasks.find({}, {sort: {text: 1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var text = event.target.text.value;
+
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text
+      });
+
+      // Clear form
+      event.target.text.value = "";
+    }
+  });
+
+  Template.task.events({
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
+}
