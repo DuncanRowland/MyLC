9c050833c43bebefbb06c7e57d0bbc61dda73fe5
Duncan Rowland <duncan.a.rowland@gmail.com>
2015-11-05 16:14:23 +0000

    Portlets added

--- MyLC.js
+++ MyLC.js
@@ -9,6 +9,29 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.task.rendered = function() {
+    //should I be here
+        $( ".column" ).sortable({
+          connectWith: ".column",
+          handle: ".portlet-header",
+          cancel: ".portlet-toggle",
+          placeholder: "portlet-placeholder ui-corner-all"
+        });
+
+
+        $( ".portlet" )
+          .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+          .find( ".portlet-header" )
+            .addClass( "ui-widget-header ui-corner-all" )
+            .prepend( "<span class='ui-icon ui-icon-plusthick portlet-toggle'></span>");
+
+        $( ".portlet-toggle" ).click(function() {
+          var icon = $( this );
+          icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
+          icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
+        });
+  }
+
   Template.body.events({
     "submit .new-task": function (event) {
       // Prevent default browser form submit
