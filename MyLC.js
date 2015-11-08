cea7def0ad8a1b60beebdcc53d2f6085007a5641
DuncanRowland <duncan.a.rowland@gmail.com>
2015-11-08 22:41:13 +0000

    Added rank info to results

--- MyLC.js
+++ MyLC.js
@@ -64,16 +64,22 @@ if (Meteor.isClient) {
     results: function () {
       // Get all votes
       var r = [];
+      var votenum = 0
       var result = Votes.find();
       result.forEach(function(entry) {
-        var text = "";
-        var id = 0;
-        while(entry[id]!=undefined) {
-          text += Items.findOne({_id: entry[id]}).text + ", ";
-          id+=1;
-        }
+        votenum += 1;
+        var text = "#"+votenum+": ";
+        var rank = 0;
+        while(entry[rank]!=undefined) {
+            _id = Items.findOne({_id: entry[rank]})
+            if(_id===undefined) {
+              text += "?, ";
+            } else {
+              text += _id.text + ", ";
+            }
+          rank+=1;
+          }
         text = text.substring(0, text.length - 2);
-        console.log(text);
         obj = {preferences: text};
         r.push(obj)
       });
