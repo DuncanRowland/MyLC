2b708ea21c98d64df0e83b5a0b414185461915f7
DuncanRowland <duncan.a.rowland@gmail.com>
2015-12-04 19:52:14 +0000

    Added google map markers

--- pages/results/client/client.js
+++ pages/results/client/client.js
@@ -24,6 +24,62 @@ Template.results.helpers({
       obj = {preferences: text, comment: commentText};
       r.push(obj);
     });
-    return r;
+//DEBUG
+    var last_element = r[r.length - 1];
+    var rr = [];
+    rr.push(last_element);
+    return rr;
+//DEBUG
+//    return r;
+  }
+});
+
+Template.googlemap.onRendered(function() {
+  GoogleMaps.load();
+});
+
+Template.googlemap.onCreated(function() {
+  GoogleMaps.ready('exampleMap', function(map) {
+    var marker1 = new google.maps.Marker({
+      position: {lat: 53.22, lng: -0.54},
+      map: map.instance,
+      url: 'http://localhost:3000/cfs/files/images/EwKzaNFZQYTPZm3w2/ben.jpg?store=images'
+    });
+    var infowindow1 = new google.maps.InfoWindow({
+      content: '<div>window 1</div>'
+    });
+    //google.maps.event.addListener(marker, 'click', function() {
+    marker1.addListener('click', function() {
+      //window.location.href = 'http://www.google.com';
+      infowindow1.open(map.instance, marker1);
+    });
+
+    var marker2 = new google.maps.Marker({
+      position: {lat: 53.22, lng: -0.55},
+      map: map.instance,
+      url: 'http://localhost:3000/cfs/files/images/EwKzaNFZQYTPZm3w2/ben.jpg?store=images'
+    });
+    var infowindow2 = new google.maps.InfoWindow({
+      content: '<div>window 2</div>'
+    });
+    //google.maps.event.addListener(marker, 'click', function() {
+    marker2.addListener('click', function() {
+      //window.location.href = 'http://www.google.com';
+      infowindow2.open(map.instance, marker2);
+    });
+
+
+  });
+});
+
+Template.googlemap.helpers({
+  exampleMapOptions: function() {
+    if (GoogleMaps.loaded()) {
+      return {
+        center: {lat: 53.22, lng: -0.54},
+//new google.maps.LatLng(53.228226, -0.546926),
+        zoom: 12
+      };
+    }
   }
 });
