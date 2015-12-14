8e15ec68fca9d13f390c2c33e9c4ef0bd01622a4
DuncanRowland <duncan.a.rowland@gmail.com>
2015-12-14 14:51:49 +0000

    Removed debug

--- pages/results/client/client.js
+++ pages/results/client/client.js
@@ -119,8 +119,7 @@ Template.googlemap.helpers({
 });
 
 Template.results.rendered = function() {
-  console.log('hello');
-(function(d, s, id) {
+  (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
