a845b9b45987495db3a1646aa067673d7605bc94
DuncanRowland <duncan.a.rowland@gmail.com>
2016-01-06 08:29:16 +0000

    Added renum.sh

---
+++ scripts/renum.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#mongoexport --host=127.0.0.1 --port=3001 --db=meteor --collection=items --out items.json
+
+c=0
+for i in {1000..1100}
+do
+   c=$((c+1))
+   l=`head -$c items.json | tail -1`
+   l1=`echo $l | cut -c-8`
+   l2=1111111111111$i
+   l3=`echo $l | cut -c26-`
+   echo $l1$l2$l3
+done
+
+#mongoimport --host=127.0.0.1 --port=3001 --db=meteor --collection=items --drop --file items.json
